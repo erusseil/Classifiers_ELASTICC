0d8ebb2bd15a31154b37d38928eefcf114ca296f
erusseil <79919110+erusseil@users.noreply.github.com>
2023-08-22 07:39:28 +0200

    Add files via upload

---
+++ reformat.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+python reformat.py 2222
+python reformat.py 2224
+python reformat.py 2226
+python reformat.py 2233
+python reformat.py 2235
+python reformat.py 2243
+python reformat.py 2245
+python reformat.py 2322
+python reformat.py 2324
+python reformat.py 2332
+python reformat.py 2223
+python reformat.py 2225
+python reformat.py 2232
+python reformat.py 2234
+python reformat.py 2242
+python reformat.py 2244
+python reformat.py 2246
+python reformat.py 2323
+python reformat.py 2325
